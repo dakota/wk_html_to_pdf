3ed6ce49eaae2f9f5e08bdd3ff453ef5a4dda128
dogmatic69 <dogmatic69@gmail.com>
2011-10-21 09:33:11 +0100

    cleaning up the code and adding some documentation to the methods

--- controllers/components/wk_html_to_pdf.php
+++ controllers/components/wk_html_to_pdf.php
@@ -1,10 +1,21 @@
 <?php
-
-class WkHtmlToPdfComponent extends Object {
-	public function initialize(&$controller) {
-		if($controller->RequestHandler->ext == 'pdf') {
-			$controller->view = 'WkHtmlToPdf.WkHtmlToPdf';
+	class WkHtmlToPdfComponent extends Object {
+		
+		/**
+		 * @brief initialise the WkHtmlToPdf Component
+		 * 
+		 * If there is an extention "pdf" in the url automatically set WkHtmlToPdf 
+		 * as the View class
+		 * 
+		 * @access public
+		 * 
+		 * @param type $controller 
+		 * 
+		 * @return void
+		 */
+		public function initialize(&$controller) {
+			if ($controller->RequestHandler->ext == 'pdf') {
+				$controller->view = 'WkHtmlToPdf.WkHtmlToPdf';
+			}
 		}
 	}
-
-}
